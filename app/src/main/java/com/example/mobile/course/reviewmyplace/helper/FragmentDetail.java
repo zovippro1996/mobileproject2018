5b7d0e4d24872cc2afcc247f866f6779cb168e2d
Trung <trunglepham1202@gmail.com>
2018-04-29 23:34:40 +0700

    Trung update estdetail 20180429 1134

---
+++ app/src/main/java/com/example/mobile/course/reviewmyplace/helper/FragmentDetail.java
@@ -0,0 +1,26 @@
+package com.example.mobile.course.reviewmyplace.helper;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.mobile.course.reviewmyplace.R;
+
+
+public class FragmentDetail extends Fragment {
+
+    public FragmentDetail(){}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
